1b8386aaa0f9e639b145e67ffa5f3fa8a22693f0
yuanye668 <31710358+yuanye668@users.noreply.github.com>
2019-08-24 06:51:20 +1200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,15 @@
-
 var express = require('express');
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
+app.use((req, res, next) => {
 
+ let string = `${req.method} ${req.path} - ${req.ip}`
+ console.log(string) 
+   
+  next();
+
+});
 
 // --> 11)  Mount the body-parser middleware  here
 
